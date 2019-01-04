a20a52a85bf1a312f9fc3e38a0d7b02040b6af1a
KhanhNV <khanhhung8889@gmail.com>
2019-01-04 11:48:46 +0700

    add end for each connection

--- routes/users.js
+++ routes/users.js
@@ -26,6 +26,7 @@ router.get('/', function (req, res, next) {
         if (error) throw error;
         let totalUsers = results[0].totalUsers;
         res.locals.connection.query('SELECT * from users order by updatedate desc LIMIT ?', Number(limit), function (error, results, fields) {
+            res.locals.connection.end();
             if (error) throw error;
             let response = {totalUsers: totalUsers, users: results};
             res.send(JSON.stringify(response));
@@ -45,6 +46,7 @@ router.post('/create', userValidator, (req, res, next) => {
         const insertSql = 'insert into users(name,age,comment,createdate,updatedate) values(?,?,?,NOW(),NOW())';
         const values = [req.body.name, req.body.age, req.body.comment];
         res.locals.connection.query(insertSql, values, function (error, results, fields) {
+            res.locals.connection.end();
             if (error) throw error;
             res.send(JSON.stringify(results));
         });
@@ -63,6 +65,7 @@ router.post('/agePlus', [check('id').not().isEmpty().withMessage('User Id is req
         console.log('plus user age sql:' + sql);
         res.locals.connection.query(sql, [req.body.id, MAX_AGE],
             function (error, results, fields) {
+                res.locals.connection.end();
                 if (error) throw error;
                 // if (results.message['Rows matched'] === '0') {
                 //     res.send(JSON.stringify({ errors: [''] }));
@@ -82,10 +85,10 @@ router.post('/delete', [check('id').not().isEmpty().withMessage('User Id is requ
     } else {
         const deleteSql = 'DELETE from users where id = ?';
         res.locals.connection.query(deleteSql, req.body.id, function (error, results, fields) {
-                if (error) throw error;
-                res.send(JSON.stringify(results));
-            }
-        );
+            res.locals.connection.end();
+            if (error) throw error;
+            res.send(JSON.stringify(results));
+        });
     }
 });
 
