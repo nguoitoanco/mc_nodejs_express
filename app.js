cb7140c7aedb8d3ecda820f8a925fee1d2007b34
KhanhNV <khanhhung8889@gmail.com>
2019-01-10 14:49:17 +0700

    Add multiple language

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ var mysql = require("mysql");
 app.use(function (req, res, next) {
     res.locals.connection = mysql.createConnection({
         host: 'localhost',
-        user: 'root',
-        password: 'root',
+        user: 'mcuser',
+        password: 'mcuser',
         database: 'node_js'
     });
     // Website you wish to allow to connect
